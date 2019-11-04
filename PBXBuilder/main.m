72b250a4125bbd3b418326322ba2a7831b3dcdf3
Galen Rhodes <galenrhodes@me.com>
2019-11-04 10:51:35 -0500

    Initial Commit

---
+++ PBXBuilder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PBXBuilder
+//
+//  Created by Galen Rhodes on 11/4/19.
+//  Copyright © 2019 Project Galen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
